c660a86532bf4caf230f90ee7ea9512130dc724e
heywhatsup88 <heywhatsup@ukr.net>
2022-11-02 18:38:50 +0300

    2nd task done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,7 +2,7 @@ const list = document.querySelectorAll("li.item");
 
 console.log(`Number of categories: ${list.length}`);
 
-const ul = document.querySelectorAll("#categories>li");
+const ul = document.querySelectorAll("li.item");
 
 ul.forEach((el) => {
   console.log(
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const elements = ingredients.map((ingredient) => {
+  const newTagEl = document.createElement("li");
+  newTagEl.textContent = ingredient;
+  newTagEl.classList.add("item");
+  return newTagEl;
+});
+
+console.log(elements);
+
+ingredientsListEl.append(...elements);
